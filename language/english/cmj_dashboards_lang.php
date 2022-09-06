a0ebb9cfba37de4e872d5ab666b3c5b520c8b50d
4ndr1sw <4ndr1sw@gmail.com>
2022-09-06 10:30:14 +0700

    lang

--- language/english/cmj_dashboards_lang.php
+++ language/english/cmj_dashboards_lang.php
@@ -9,7 +9,7 @@ $lang['uncompletesd_tasks_for']                                                 
 $lang['no_tasks_listed']                                                         = 'There is not tasks listed';
 $lang['tasks_not_assigned']                                                      = 'Tasks not assigned';
 $lang['projects_have_no_tasks']                                                  = 'Projects have no tasks';
-$lang['completed_tasks_no_file']                                                 = 'Tugas selesai tidak ada file';
+$lang['completed_tasks_no_file']                                                 = 'Completed tasks no file';
 $lang['staff_with_statuses']                                                 = 'Staff with statuses information';
 $lang['project_tasks_no_tags']                                                 = 'Project tasks no tags';
 
--- language/indonesia/cmj_dashboards_lang.php
+++ language/indonesia/cmj_dashboards_lang.php
@@ -2,7 +2,8 @@
 
 # Version 2.3.0
 
-$lang['clients_task_statuses']                                          = 'Client task status';
+$lang['clients_task_statuses']                                          = 'Status task client';
+$lang['clients_task_recapitulation']                                    = 'Recapitulasi task client';
 $lang['uncomplete_tasks']                                               = 'Tasks belum lengkap';
 $lang['uncompletesd_tasks_for']                                         = 'Tasks belum lengkap untuk';
 $lang['no_tasks_listed']                                                = 'Tidak ada tugas yang terdaftar';
@@ -10,3 +11,4 @@ $lang['tasks_not_assigned']                                             = 'Tugas
 $lang['projects_have_no_tasks']                                         = 'Projects tidak memiliki tugas';
 $lang['staff_with_statuses']                                                 = 'Staf dengan informasi status';
 $lang['project_tasks_no_tags']                                                 = 'Tasks tidak ada tags';
+$lang['completed_tasks_no_file']                                                 = 'Tugas selesai tidak ada file';
